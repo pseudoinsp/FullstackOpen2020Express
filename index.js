a96cd200f5861d98019684219a83cf5f7dbd969d
Endre Berki <berkiendre@gmail.com>
2020-05-10 14:04:29 +0200

    3.2 - 3.5 - person addressing, deletion, info page

--- index.js
+++ index.js
@@ -29,13 +29,41 @@ let persons = [
       }
 ]
 
+const ComposeServerInfo = () => {
+    return `Phonebook has info for ${persons.length} people <br/> <br/> 
+            ${new Date()}`
+}
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    res.send(ComposeServerInfo())
 })
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
