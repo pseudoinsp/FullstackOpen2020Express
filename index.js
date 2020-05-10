f755a7af2a423691142cb0a2647ad6c0ac13e43f
Endre Berki <berkiendre@gmail.com>
2020-05-10 13:38:35 +0200

    3.1 - Add express, nodemon, persons endpoint

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      },
+      {
+        "name": "asd",
+        "number": "45345",
+        "id": 5
+      }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
