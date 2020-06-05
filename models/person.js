c49939e82c4a0a53d4bdc663b7702fe8db54f113
Endre Berki <berkiendre@gmail.com>
2020-06-05 22:13:28 +0200

    3.20 - Add minimum length validation for person name and number

--- models/person.js
+++ models/person.js
@@ -14,9 +14,9 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-    id: {type: Number, unique: true },
-    name: { type: String, unique: true },
-    number: String
+    id: { type: Number, unique: true },
+    name: { type: String, unique: true, minlength: 3 },
+    number: {type: String, minlength: 8 }
 })
 personSchema.plugin(uniqueValidator)
 
